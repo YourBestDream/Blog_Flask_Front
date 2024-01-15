97838d1950900128e0052015284fd76f9acb1448
YourBestDream <ferari100w3@gmail.com>
2024-01-15 23:54:56 +0200

    Added submenu for account field in navbar

--- src/modules/AuthContext.js
+++ src/modules/AuthContext.js
@@ -5,23 +5,7 @@ export const AuthContext = createContext()
 
 export const AuthProvider = ({children}) =>{
     const [isLoggedIn,setLoggedIn] = useState(false)
-
-    // useEffect (() => {
-    //     const fetchLoggingStatus = async () =>{
-    //         try{
-    //             const response = await axios.get('/auth/check', { withCredentials: true })
-    //             setLoggedIn(response.data.logged)
-    //         } catch (error){
-    //             console.error('Error fetching status: ', error)
-    //         }
-    //     }
-    //     fetchLoggingStatus()
-    // },[])
-
-    // useEffect(() => {
-    //     console.log(isLoggedIn);
-    // }, [isLoggedIn]);
-
+    
     const updateLoginStatus = async () => {
         try {
             const response = await axios.get('/auth/check', { withCredentials: true });
